fc5f27d1848341319f174134778315d724126ac3
Sakolkrit Pengkhum <93993627+Sakolkrit@users.noreply.github.com>
2023-09-21 23:36:05 +0700

    change bloodgroup.c

--- bloodgroup.c
+++ bloodgroup.c
@@ -44,5 +44,5 @@ void main()
     printf("\nContinue (YES for Yes)? ");
     scanf("%s", answer);
   } while (strcmp(answer, "YES") == 0);
-  printf("Goodbye\n");
+  printf("Goodbye Everyone\n");
 }
